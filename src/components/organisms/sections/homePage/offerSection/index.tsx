92346cdc955b25e9dc97435354717d91f7281d55
Karol <KarolKaczorowski@interia.pl>
2020-12-10 00:16:33 +0100

    finished offers section

--- src/components/organisms/sections/homePage/offerSection/index.tsx
+++ src/components/organisms/sections/homePage/offerSection/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PrimitivesContext } from '../../../../../contexts/primitivesContext';
 import { SectionWithId }  from '../../../../atoms/section';
+import { Offers } from '../../../../../websiteTextContent/HomePage';
+import OfferAdvertisement from '../../../../molecues/OfferAdvertisement';
 
 export default function OfferSection() {
   
+  const { currentLanguage } = useContext(PrimitivesContext);
+  const isEnglish = currentLanguage === 'ENGLISH';
 
   return (
     <SectionWithId id="offer-section">
       {/* 1. Mapped offers */}
+      { Offers.map(({ headling, p, linkContent, ...rest }) => 
+        <OfferAdvertisement 
+          headling={ isEnglish ? headling.eng : headling.pl }
+          p={ isEnglish ? p.eng : p.pl }
+          linkContent={ isEnglish ? linkContent.eng : linkContent.pl }
+          { ...rest }
+        />) 
+      }
     </SectionWithId>
   )
 }
