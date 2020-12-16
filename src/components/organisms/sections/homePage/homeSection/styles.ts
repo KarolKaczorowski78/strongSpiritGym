adf795e3d14ac244fed7718ce5a114a5be058881
Karol <KarolKaczorowski@interia.pl>
2020-12-16 10:57:07 +0100

    udated home section styling

---
+++ src/components/organisms/sections/homePage/homeSection/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components';
+import H1Proto from '../../../../atoms/h1';
+import ImgProto from '../../../../atoms/img';
+import SpanProto from '../../../../atoms/span';
+
+export const H1 = styled(H1Proto)`
+  font-size: 4rem;
+  text-transform: uppercase;
+  text-shadow: 0 0 10px black;
+  color: lightgrey;
+  margin-top: 100px;
+`;
+
+export const Span = styled(SpanProto)`
+  display: block;
+`;
+
+export const Img = styled(ImgProto)`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  object-position: top right;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+export const ButtonStyling = css`
+  position: absolute;
+  bottom: 10px;
+  width: 150px;
+  height: auto;
+  left: 50%;
+  background: none;
+  border: none;
+  margin-left: -75px;
+  padding-top: 16px;
+  & > svg {
+    color: lightgrey;
+    font-size: 30px;
+    display: block;
+    margin: -17px auto 0;
+  }
+`;
