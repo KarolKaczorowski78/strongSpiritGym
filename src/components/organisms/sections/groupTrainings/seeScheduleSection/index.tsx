3cdbe50a063c897d7d426f5e760ccbf2de0a963d
Karol <KarolKaczorowski@interia.pl>
2020-12-17 12:20:19 +0100

    added SeeSchedule section

---
+++ src/components/organisms/sections/groupTrainings/seeScheduleSection/index.tsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { PrimitivesContext } from '../../../../../contexts/primitivesContext';
+import { SectionWithId } from '../../../../atoms/section/index';
+import { ESectionIds } from '../../../../../__types__/ESectionsIds';
+import { Img, SectionStyling, TextContainer, H1, Link } from './styles';
+import { SeeScheduleSection as Content } from '../../../../../websiteTextContent/GroupTrainingsPage';
+import { ECssUnits } from '../../../../../__types__/ECssUnits';
+import { EBreakpoints } from '../../../../../__types__/EBreakpoints';
+import { removeUnitAndParseInt } from '../../../../../universal/removeUnitAndParseInt';
+import P from '../../../../atoms/p';
+import { EChildRoutes } from '../../../../../__types__/ERoutes';
+
+const { headling, linkContent, imgBig, imgSmall, p } = Content;
+
+export default function SeeScheduleSection() {
+  
+  const { currentLanguage, windowWidth } = useContext(PrimitivesContext);
+  const isEnglish = currentLanguage === 'ENGLISH';
+
+  return (
+    <SectionWithId id={ ESectionIds.GROUP_WORKOUTS_SCHEDULE } styling={ SectionStyling }>
+      <Img
+        src={ 
+          removeUnitAndParseInt(EBreakpoints.TABLET_SMALL, ECssUnits.pixels) < windowWidth ? 
+          imgBig : imgSmall
+        }
+      />
+      <TextContainer>
+        <H1>
+          { isEnglish ? headling.eng : headling.pl }
+        </H1>
+        <P>
+          {
+            isEnglish ? p.eng : p.pl
+          }
+        </P>
+        <Link to={ EChildRoutes.SCHEDULE }>
+          {
+            isEnglish ? linkContent.eng : linkContent.pl
+          }
+        </Link>
+      </TextContainer>
+    </SectionWithId>
+  )
+}
---
+++ src/components/organisms/sections/groupTrainings/seeScheduleSection/styles.ts
@@ -0,0 +1,63 @@
+import styled, { css, keyframes } from 'styled-components';
+import ImgProto from '../../../../atoms/img';
+import { EBreakpoints } from '../../../../../__types__/EBreakpoints';
+import Div from '../../../../atoms/div';
+import H1Proto from '../../../../atoms/h1';
+import LinkProto from '../../../../atoms/link';
+
+export const pulseEffect = keyframes`
+  0% {
+    transform: scale(1.03)
+  }
+  70% {
+    transform: scale(.97)
+  }
+  100% {
+    transform: scale(1.03)
+  }
+`;
+
+export const SectionStyling = css`
+  height: calc(100vh - 100px);
+  min-height: 300px;
+`;
+
+export const Img = styled(ImgProto)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const TextContainer = styled(Div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, .5);
+  flex-direction: column;
+  * {
+    color: lightgrey;
+    margin: 20px 0;
+  }
+`;
+
+export const H1 = styled(H1Proto)`
+  font-size: 4rem;
+  @media (max-width: ${EBreakpoints.TABLET_SMALL}) {
+    font-size: 2rem;
+  }
+`;
+
+export const Link = styled(LinkProto)`
+  margin-top: 30px;
+  font-size: 2rem;
+  text-transform: uppercase;
+  border: 2px solid lightgrey;
+  padding: 15px 20px;
+  background: rgba(235, 20, 20, .2);
+  animation: ${pulseEffect} 1s ease-out infinite;
+`
