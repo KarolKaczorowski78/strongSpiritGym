e41b315c5f0b2e4881bf095480dc3a8bcb9a43e2
Karol <KarolKaczorowski@interia.pl>
2020-12-28 13:23:21 +0100

    added locations homepage content

---
+++ src/websiteTextContent/Locations.ts
@@ -0,0 +1,14 @@
+export const Home = {
+  headling: {
+    eng: 'Find a gym in your city',
+    pl: 'Znajdz klub w swoim miescie',
+  },
+  input: {
+    eng: 'Your city',
+    pl: 'Twoje miasto'
+  },
+  button: {
+    eng: 'List of countries',
+    pl: 'Lista panstw',
+  }
+}
