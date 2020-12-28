61fdf5adfe4630f39223185ba78e4aae01e0cdeb
Karol <KarolKaczorowski@interia.pl>
2020-12-28 15:26:10 +0100

    updated locations view

--- src/views/locations.tsx
+++ src/views/locations.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { UIEvent, useState } from 'react';
+import styled from 'styled-components';
 import Page from '../templates/page';
 import HomeSection from '../components/organisms/sections/locations/homeSection';
+import { LocationsContents } from '../websiteTextContent/Locations';
+import LocationSection from '../components/organisms/sections/locations/locationSection';
+import Div from '../components/atoms/div';
+
+const Wrapper = styled(Div)`
+  height: calc(100vh - 100px);
+  width: 100%;
+  overflow-y: scroll;
+  min-height: 400px;
+  margin-top: 100px;
+  transition: 2s;
+`;
 
 export default function Locations() {
+
+  const [scrollValue, setScrollValue] = useState<number>(0);
+  const [isScrolling, setIsScrolling] = useState<boolean>(false);
   
+  const handleScroll = async (e: UIEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLDivElement;
+  }
 
   return (
     <Page>
-      <HomeSection />
+      <Wrapper onScroll={ handleScroll } id="locations-wrapper">
+        <HomeSection />
+        { LocationsContents.map(location => <LocationSection { ...location } />) }
+      </Wrapper>
     </Page>
   )
 }
