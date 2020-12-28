3a6a8ed1b397c22793b6f1721f89a3cc9a3db342
Karol <KarolKaczorowski@interia.pl>
2020-12-28 15:26:01 +0100

    updated LocationSection component

--- src/components/organisms/sections/locations/locationSection/index.tsx
+++ src/components/organisms/sections/locations/locationSection/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { FC, useContext } from 'react';
+import { PrimitivesContext } from '../../../../../contexts/primitivesContext';
 import { SectionWithId } from '../../../../atoms/section';
+import { SectionStyling, Img, TextContainer, H2 } from './styles';
+import ILocationSection from '../../../../../__types__/ILocationSection';
+import FancyLink from '../../../../molecues/FancyLink2';
+import P from '../../../../atoms/p';
 
-const LocationSection = () => {
+const LocationSection: FC<ILocationSection> = ({ id, img, countryName, description, linkPath }) => {
+
+  const { currentLanguage } = useContext(PrimitivesContext);
+  const isEnglish = currentLanguage === 'ENGLISH';
+
   return (
-    <section>
-
-    </section>
+    <SectionWithId id={ id } styling={ SectionStyling }>
+      <Img src={ img } alt="" />
+      <TextContainer>
+        <H2>{ isEnglish ? countryName.eng : countryName.pl }</H2>
+        <P>{ isEnglish ? description.eng : description.pl }</P>
+        <FancyLink 
+          linkContent={ isEnglish ? 'Find a club' : 'Znajdz klub' }
+          linkPath={ linkPath }
+          fontSizeRem={ 1.8 }
+        />
+      </TextContainer>
+    </SectionWithId>
   )
 }
 
--- src/components/organisms/sections/locations/locationSection/styles.ts
+++ src/components/organisms/sections/locations/locationSection/styles.ts
@@ -1,1 +1,39 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import ImgProto from '../../../../atoms/img';
+import DivProto from '../../../../atoms/div';
+import H2Proto from '../../../../atoms/h2';
+
+export const SectionStyling = css`
+  height: calc(100vh - 100px);
+  min-height: 400px;
+`;
+
+export const Img = styled(ImgProto)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: bottom;
+`;
+
+export const TextContainer = styled(DivProto)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, .6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  & > * {
+    margin: 25px 0;
+  }
+  * {
+    color: lightgrey;
+  }
+`;
+
+export const H2 = styled(H2Proto)`
+  font-size: 3rem;
+`;
