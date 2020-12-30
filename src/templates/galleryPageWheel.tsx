21cda1ce391e277b3b3e3a95c26c0be00c671a32
Karol <KarolKaczorowski@interia.pl>
2020-12-30 19:21:23 +0100

    added keydown functionality to GalleryPageWheel template

--- src/templates/galleryPageWheel.tsx
+++ src/templates/galleryPageWheel.tsx
@@ -8,23 +8,43 @@ const GalleryPageWheel: FC<IGalleryPage> = ({ sectionsIds, currSectionIndex, set
     window.removeEventListener('wheel', handleWheel);
     const direction = e.deltaY > 0 ? 1 : -1;
 
-    const scrollTargetIndex = (direction === - 1 && currSectionIndex === 0) || (direction === 1 && currSectionIndex === sectionsIds.length - 1) ? currSectionIndex : currSectionIndex + direction;
-    
-    scrollTargetIndex !== currSectionIndex ? setCurrSectionIndex(() => scrollTargetIndex) : window.addEventListener('wheel', handleWheel);
+    finish(direction, 'wheel');
+  }
 
+  const handleKeyDown = ({ code }: KeyboardEvent) => {
+    if (code === 'ArrowUp' || code === 'ArrowDown') {
+      window.removeEventListener('keydown', handleKeyDown);
+      const direction = code === 'ArrowUp' ? -1 : 1;
+
+      finish(direction, 'keydown');
+    }
+  }
+
+  const finish = (direction: -1 | 1, event: 'wheel' | 'keydown') => {
+    const scrollTargetIndex = (direction === - 1 && currSectionIndex === 0) || (direction === 1 && currSectionIndex === sectionsIds.length - 1) ? currSectionIndex : currSectionIndex + direction;
+
+    if (scrollTargetIndex !== currSectionIndex) {
+      setCurrSectionIndex(() => scrollTargetIndex)  
+    } else {
+      event === 'keydown' ? window.addEventListener('keydown', handleKeyDown) : window.addEventListener('wheel', handleWheel);
+    }
   }
 
   useEffect(() => {
     scrollToSection(sectionsIds[currSectionIndex]);
+    
     window.addEventListener('wheel', handleWheel);
+    window.addEventListener('keydown', handleKeyDown);
   }, [currSectionIndex])
 
   useEffect(() => {
     window.addEventListener('wheel', handleWheel);
+    window.addEventListener('keydown', handleKeyDown)
     document.body.style.overflow = 'hidden';
 
     return () => {
       document.body.style.overflow = 'auto';
+      window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('wheel', handleWheel);
     }
   }, [])
