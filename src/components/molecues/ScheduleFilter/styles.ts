47dac3eb4601cdbcb345337cfad95e748349d3ea
Karol <KarolKaczorowski@interia.pl>
2021-01-01 20:45:04 +0100

    added ScheduleFilter

---
+++ src/components/molecues/ScheduleFilter/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  background: black;
+  border: none;
+  margin: 15px 0;
+  padding: 8px 5px;
+  border: 1px solid lightgrey;
+`;
