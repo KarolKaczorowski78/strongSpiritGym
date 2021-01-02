b7013aad5e8d3b8e49460d46ac39803502ddfcae
Karol <KarolKaczorowski@interia.pl>
2021-01-02 17:36:32 +0100

    updated IndividualPrograms page

--- src/views/individualPrograms.tsx
+++ src/views/individualPrograms.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Page from '../templates/page';
+import HomeSection from '../components/organisms/sections/individualPrograms/homeSection';
 
 export default function Individualprograms() {
   
 
   return (
     <Page>
-      <h1>Individual programs</h1>
+      {/* <h1>Individual programs</h1> */}
+      <HomeSection />
     </Page>
   )
 }
