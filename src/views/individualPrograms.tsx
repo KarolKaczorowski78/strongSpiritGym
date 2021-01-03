8422a19d616fc73344839a7446dd5b4f4a71f252
Karol <KarolKaczorowski@interia.pl>
2021-01-03 14:14:45 +0100

    implemented changes into IndividualPrograms view

--- src/views/individualPrograms.tsx
+++ src/views/individualPrograms.tsx
@@ -3,12 +3,14 @@ import Page from '../templates/page';
 import HomeSection from '../components/organisms/sections/individualPrograms/homeSection';
 import { Sections } from '../websiteTextContent/IndividualPrograms';
 import ProgramSection from '../components/organisms/sections/individualPrograms/programSection';
+import AboutSection from '../components/organisms/sections/individualPrograms/aboutSection';
 
 export default function Individualprograms() {
 
   return (
     <Page>
       <HomeSection />
+      <AboutSection />
       { Sections.map(section => <ProgramSection { ...section } />) }
     </Page>
   )
