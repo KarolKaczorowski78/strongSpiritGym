30ffa7f36b34d8f9bd54bf0b3b1af4de00f67d40
Karol <KarolKaczorowski@interia.pl>
2021-01-03 13:17:16 +0100

    updated WebsiteTextContent

--- src/websiteTextContent/IndividualPrograms.ts
+++ src/websiteTextContent/IndividualPrograms.ts
@@ -1,5 +1,12 @@
+import { ESectionIds } from '../__types__/ESectionsIds';
 import IContent from '../__types__/IContent';
+import IProgramSection from '../__types__/IProgramSection';
 import { loremIpsum } from './loremIpsum';
+import MmaImg from '../img/individualPrograms/mma.jpg';
+import GroundGameImg from '../img/individualPrograms/groundGame.jpg';
+import StandUpImg from '../img/individualPrograms/standUp.jpg';
+import PowerliftingImg from '../img/individualPrograms/powerlifting.jpg';
+import { EChildRoutes, EMainRoutes } from '../__types__/ERoutes';
 
 export const headling = {
   firstLine: {
@@ -41,5 +48,48 @@ export const words: IContent[] = [
   {
     eng: 'power',
     pl: 'moc',
+  }
+]
+
+export const Sections: IProgramSection[] = [
+  {
+    id: ESectionIds.INDIVIDUAL_PROGRAMS_MMA,
+    img: MmaImg,
+    name: {
+      eng: 'MMA',
+      pl: 'MMA',
+    },
+    linkPath: EMainRoutes.INDIVIDUAL_PROGRAMS,
+    strengths: [.6, .7, .7, .9],
+  },
+  {
+    id: ESectionIds.INDIVIDUAL_PROGRAMS_POWERLIFTING,
+    img: PowerliftingImg,
+    name: {
+      eng: 'Powerlifting',
+      pl: 'Powerlifting',
+    },
+    linkPath: EMainRoutes.INDIVIDUAL_PROGRAMS,
+    strengths: [.9, .6, .4, .6],
+  },
+  {
+    id: ESectionIds.INDIVIDUAL_PROGRAMS_STAND_UP,
+    img: StandUpImg,
+    name: {
+      eng: 'Stand up',
+      pl: 'Walka w stójce',
+    },
+    linkPath: EMainRoutes.INDIVIDUAL_PROGRAMS,
+    strengths: [.4, .7, .9, .9],
+  },
+  {
+    id: ESectionIds.INDIVIDUAL_PROGRAMS_GROUND_GAME,
+    img: GroundGameImg,
+    name: {
+      eng: 'Ground game',
+      pl: 'Walka w parterze',
+    },
+    strengths: [.7, .7, .5, .6],
+    linkPath: EMainRoutes.INDIVIDUAL_PROGRAMS,
   }
 ]
