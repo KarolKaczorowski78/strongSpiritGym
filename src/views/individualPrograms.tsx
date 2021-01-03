fa3230fea4e33c9177d0cd76ad5e2099e81ccb41
Karol <KarolKaczorowski@interia.pl>
2021-01-03 13:17:33 +0100

    implemented changes into IndividualPrograms view

--- src/views/individualPrograms.tsx
+++ src/views/individualPrograms.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Page from '../templates/page';
 import HomeSection from '../components/organisms/sections/individualPrograms/homeSection';
+import { Sections } from '../websiteTextContent/IndividualPrograms';
+import ProgramSection from '../components/organisms/sections/individualPrograms/programSection';
 
 export default function Individualprograms() {
-  
 
   return (
     <Page>
-      {/* <h1>Individual programs</h1> */}
       <HomeSection />
+      { Sections.map(section => <ProgramSection { ...section } />) }
     </Page>
   )
 }
