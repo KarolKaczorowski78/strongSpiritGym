35b3cce5d2191cb0819e155a9226d7d8b6425925
Karol <KarolKaczorowski@interia.pl>
2021-01-06 15:55:42 +0100

    added IndividualProgram website content file

---
+++ src/websiteTextContent/IndividualProgram.ts
@@ -0,0 +1,41 @@
+import IIndividualProgramPage from '../__types__/IIndividualProgramPage';
+import TestImg from '../img/individualProgram/test.jpg';
+
+export const individualProgramPages: IIndividualProgramPage[] = [
+  {
+    backgroundImg: TestImg,
+    homeSection: {
+      headling: {
+        eng: 'Learn how to fight in every area',
+        pl: 'Naucz sie walki w kazdej plaszczyznie',
+      }
+    }
+  },
+  {
+    backgroundImg: TestImg,
+    homeSection: {
+      headling: {
+        eng: 'Develop strength and great physique',
+        pl: 'Zbuduj siłe i wspaniala sylwetke',
+      }
+    }
+  },
+  {
+    backgroundImg: TestImg,
+    homeSection: {
+      headling: {
+        eng: 'Learn how to strike with incredible precision',
+        pl: 'Naucz sie uderzac z nadludzka precyzja',
+      }
+    }
+  },
+  {
+    backgroundImg: TestImg,
+    homeSection: {
+      headling: {
+        eng: 'Master the art of the ground fight',
+        pl: 'Zostan mistrzem walki w parterze',
+      }
+    }
+  },
+]
