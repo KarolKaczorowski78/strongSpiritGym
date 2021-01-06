d1bba066fc996a5de6cf5f23e3a59e43d95bdfca
Karol <KarolKaczorowski@interia.pl>
2021-01-06 20:48:14 +0100

    added IndividualProgramAboutSection interface

---
+++ src/__types__/IIndividualProgramAboutSection.ts
@@ -0,0 +1,6 @@
+import IIndividualProgramArticle from "./IIndividualProgramArticle";
+
+export default interface IIndividualProgramAboutSection {
+  aboutDiscipline: IIndividualProgramArticle,
+  aboutProgram: IIndividualProgramArticle,
+}
