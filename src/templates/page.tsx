d85f6babfb0ffad80a3e7a657d4dcdccd92ba149
Karol <KarolKaczorowski@interia.pl>
2021-01-13 18:42:10 +0100

    implemented SmallTimer component into Page Template

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -1,6 +1,9 @@
-import React, { FC, ReactNode, useEffect } from 'react';
+import React, { FC, ReactNode, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import Div from '../components/atoms/div';
+import { TimerContext } from '../contexts/timerContext';
+import { useLocation } from 'react-router-dom';
+import SmallTimer from '../components/organisms/Timer/SmallTimer';
 
 const PageTemplate = styled(Div)`
   width: 100%;
@@ -10,12 +13,16 @@ const PageTemplate = styled(Div)`
 
 const Page: FC<{ children: ReactNode }> = ({ children }) => {
 
+  const { runningStatus } = useContext(TimerContext);
+  const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [])
 
   return (
     <PageTemplate>
+      { (!pathname.includes('timer') && runningStatus) && <SmallTimer /> }
       { children }
     </PageTemplate>
   )
