c47c5c0b274079a72a2a0a04be4afa187b9ccdc3
Karol <KarolKaczorowski@interia.pl>
2021-01-13 18:42:22 +0100

    updated Timer view

--- src/views/timer.tsx
+++ src/views/timer.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Page from '../templates/page';
+import { TimerContext } from '../contexts/timerContext';
+import SettingView from '../components/organisms/Timer/SettingView';
+import TrainingView from '../components/organisms/Timer/TrainingView';
 
 export default function Timer() {
   
+  const { runningStatus } = useContext(TimerContext);
 
   return (
     <Page>
-      <h1>Timer</h1>
+      { runningStatus ? <TrainingView /> : <SettingView /> }
     </Page>
   )
 }
