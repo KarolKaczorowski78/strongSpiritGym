ade2ff2bd7a0a1148dc4f4fe850ef2893a9312b0
Karol <KarolKaczorowski@interia.pl>
2021-01-14 12:14:10 +0100

    implemented a faster timer setting functionality

--- src/components/molecues/TimerSetter/index.tsx
+++ src/components/molecues/TimerSetter/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useContext } from 'react';
+import { PrimitivesContext } from '../../../contexts/primitivesContext';
 import { Container, Button, ButtonsContainer, TimeContainer } from './styles';
 import ITimeSetter from '../../../__types__/ITimeSetter';
 import Div from '../../atoms/div';
@@ -6,11 +7,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const TimerSetter: FC<ITimeSetter> = ({ state, content, setState, setStateValue, maxStateValue }) => {
+  
+  const { isTouchDevice } = useContext(PrimitivesContext);
+  const eventTimeout = useRef<number>(0);
+  const stateInterval = useRef<number>(0);
+
+  const handleMouseDown = (action: () => void) => {
+    action();
+    
+    eventTimeout.current = setTimeout(() => {
+      stateInterval.current = setInterval(() => {
+        action();
+      }, 50);
+    }, 1000);
+  }
+
+  const handleMouseUp = () => {
+    clearTimeout(eventTimeout.current);
+    clearInterval(stateInterval.current);
+  }
+
+  const actionDecrease = () => {
+    setState(previous => previous === setStateValue ? previous : previous - setStateValue);
+  }
+
+  const actionIncrease = () => {
+    setState(previous => previous === maxStateValue ? previous : previous + setStateValue)
+  }
+  
   return (
     <Container>
       <Div>{ content }</Div>
       <ButtonsContainer>
-        <Button onClick={ () => { setState(previous => previous === setStateValue ? previous : previous - setStateValue) } }>
+        <Button 
+          onMouseDown={ () => !isTouchDevice && handleMouseDown(actionDecrease) }
+          onTouchStart={ () => handleMouseDown(actionDecrease) }
+          onMouseUp={ handleMouseUp } 
+          onTouchEnd={ handleMouseUp }
+        >
           <FontAwesomeIcon icon={ faMinus } />
         </Button>
         &nbsp;&nbsp;&nbsp;
@@ -18,7 +52,12 @@ const TimerSetter: FC<ITimeSetter> = ({ state, content, setState, setStateValue,
           { state }
         </TimeContainer>
         &nbsp;&nbsp;&nbsp;
-        <Button onClick={ () => { setState(previous => previous === maxStateValue ? previous : previous + setStateValue) } }>
+        <Button 
+          onMouseDown={ () => !isTouchDevice && handleMouseDown(actionIncrease) }
+          onTouchStart={ () => handleMouseDown(actionIncrease) }
+          onMouseUp={ handleMouseUp }  
+          onTouchEnd={ handleMouseUp } 
+        >
           <FontAwesomeIcon icon={ faPlus } />
         </Button>
       </ButtonsContainer>
