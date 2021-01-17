f213b2c9e85048aca4997cd226fa8e8cbd3dafc2
Karol <KarolKaczorowski@interia.pl>
2021-01-17 21:46:46 +0100

    updated FreePerks view

--- src/views/freePerks.tsx
+++ src/views/freePerks.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import Page from '../templates/page';
-import MobileContainer from '../components/molecues/MobileContainer';
 import { Sections } from '../websiteTextContent/TrainingPerks';
-import TrainingPerkSection from '../components/organisms/sections/trainingPerks/trainingPerk';
-import TimerImage from '../img/trainingPerks/timer.png';
-import TimerImage2 from '../img/trainingPerks/timer2.png';
-import CalcImage from '../img/trainingPerks/calculator.png';
-import CalcImage2 from '../img/trainingPerks/calculator2.png';
 import { PrimitivesContext } from '../contexts/primitivesContext';
 import TrainingPerk from '../components/organisms/sections/trainingPerks/trainingPerk';
 
