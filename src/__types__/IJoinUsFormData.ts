b574f0bc9c3ccb69c91dc4dae693d52fc30c35dc
Karol <KarolKaczorowski@interia.pl>
2021-01-18 13:14:38 +0100

    added IJoinUsFormData interface

---
+++ src/__types__/IJoinUsFormData.ts
@@ -0,0 +1,9 @@
+import { EIndividualProgramSports } from './EIndividualProgramSports';
+
+export default interface IJoinUsFormData {
+  first_name: string,
+  last_name: string,
+  about_experience: string,
+  about: string,
+  specialization: EIndividualProgramSports,
+}
