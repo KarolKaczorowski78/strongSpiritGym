c261332f247c9ceeb3892b54d536f31b252ebb6b
Karol <KarolKaczorowski@interia.pl>
2021-01-20 12:49:14 +0100

    fixed TimrButton component (removed click event on touch devices)

--- src/components/molecues/TimerButton/index.tsx
+++ src/components/molecues/TimerButton/index.tsx
@@ -1,17 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { PrimitivesContext } from '../../../contexts/primitivesContext';
 import ITimerButton from '../../../__types__/ITimerButton';
 import { Button } from './styles';
 
 const TimerButton: FC<ITimerButton> = ({ icon, action }) => {
 
+  const { isTouchDevice } = useContext(PrimitivesContext);
   const handleClick = () => {
     action();
   }
 
   // added onTouchEnd event handler so that it works with Draggable component on touch devices
   return (
-    <Button onClick={ handleClick } onTouchEnd={ handleClick }> 
+    <Button { ...isTouchDevice ? { onTouchEnd: handleClick } : { onClick: handleClick } } > 
       <FontAwesomeIcon icon={ icon } />
     </Button>
   )
